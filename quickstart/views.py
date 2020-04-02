9c9ef3b1735dd52d6cfccff7b874e72ca93db2a1
Enrique Galindo <egalindo@protonmail.com>
2020-04-01 23:10:59 -0400

    deleted bad comments

--- quickstart/views.py
+++ quickstart/views.py
@@ -6,33 +6,21 @@ from .serializers import ManufacturerSerializer, ShoeTypeSerializer, ShoeColorSe
 
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializer
 
 
 class ShoeTypeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = ShoeType.objects.all()
     serializer_class = ShoeTypeSerializer
 
 
 class ShoeColorViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = ShoeColor.objects.all()
     serializer_class = ShoeColorSerializer
 
 
 class ShoeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Shoe.objects.all()
     serializer_class = ShoeSerializer
 
